6752dfee7c831d88ef0438463d3c520ed44f675c
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2024-02-01 14:55:56 +0700

    update metadat

--- src/Component/Navbar/index.jsx
+++ src/Component/Navbar/index.jsx
@@ -1,12 +1,11 @@
 import React, { memo } from "react";
 import s from "./Navbar.module.scss";
-import logo from "../../assets/datasintensa.jpeg";
+
 
 const NavBar = () => (
   <div className={s.wrapper}>
     <div className={s.container}>
-      <img src={logo}/>
-      <a href="/">Interview Test Datasintesa</a>
+      <a href="/">Mapbox API Playground</a>
       {/* <a href="/map">Map</a>/ */}
     </div>
   </div>
